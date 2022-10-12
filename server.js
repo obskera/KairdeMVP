b576cbbc6ea809d2632528699c28b5df23742e85
obskera <vivifydecepi@gmail.com>
2022-10-12 15:48:41 -0700

    sever stability changes

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 const express = require("express");
 const app = express();
+
+//new stuff
+const { MongoClient } = require('mongodb');
+const uri = process.env.DB_STRING;
+const client = new MongoClient(uri);
+//
+
 const mongoose = require("mongoose");
 const passport = require("passport");
 const session = require("express-session");
@@ -7,7 +14,7 @@ const MongoStore = require("connect-mongo")(session);
 const methodOverride = require("method-override");
 const flash = require("express-flash");
 const logger = require("morgan");
-const connectDB = require("./config/database");
+// const connectDB = require("./config/database");
 const mainRoutes = require("./routes/main");
 const postRoutes = require("./routes/posts");
 const commentRoutes = require("./routes/comments");
@@ -59,8 +66,19 @@ app.use("/", mainRoutes);
 app.use("/post", postRoutes);
 app.use("/comment", commentRoutes);
 
-//Server Running
-const PORT = 3000
-app.listen(process.env.PORT || PORT, () => {
-  console.log("Server is running, you better catch it!");
+//Server Running OLD CONNECT CODE
+// const PORT = 3000
+// app.listen(process.env.PORT || PORT, () => {
+//   console.log("Server is running, you better catch it!");
+// });
+
+//new stuff
+client.connect(err => {
+  if(err){ console.error(err); return false;}
+  // connection to mongo is successful, listen for requests
+  app.listen(3000, () => {
+      console.log("listening for requests");
+  })
 });
+
+//
