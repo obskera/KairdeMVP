69ecce880d72d387b2cb2a626461c4d78133b608
obskera <vivifydecepi@gmail.com>
2022-10-13 04:18:57 -0700

    trying change

--- config/database.js
+++ config/database.js
@@ -1,3 +1,20 @@
+// const mongoose = require("mongoose");
+// const connectDB = async () => {
+//   try {
+//     const conn = await mongoose.connect(process.env.DB_STRING, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//       useFindAndModify: false,
+//       useCreateIndex: true,
+//     });
+//     console.log(`MongoDB Connected: ${conn.connection.host}`);
+//   } catch (err) {
+//     console.error(err);
+//     process.exit(1);
+//   }
+// };
+// module.exports = connectDB;
+
 const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
@@ -6,8 +23,14 @@ const connectDB = async () => {
       useUnifiedTopology: true,
       useFindAndModify: false,
       useCreateIndex: true,
-    });
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    }).then(_ => {
+      if (conn) {
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+      } else {
+        return new Error('failed to connect')
+      }
+    })
+   
   } catch (err) {
     console.error(err);
     process.exit(1);
