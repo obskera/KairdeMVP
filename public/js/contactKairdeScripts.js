c9e2ced7153c66e22762511a7cb32a57224707d6
obskera <vivifydecepi@gmail.com>
2022-10-14 20:07:52 -0700

    testing manual click assignment

--- public/js/contactKairdeScripts.js
+++ public/js/contactKairdeScripts.js
@@ -63,10 +63,10 @@ const elements = document.querySelectorAll('.unpress')
 const dummy = document.getElementById('dummy')
 Array.from(elements).forEach(function(ele) {
   ele.addEventListener('click', () => {
+    ele.style.color = 'black'
     setTimeout(() => {
-
       ele.style.color = 'white'
       console.log('success!', ele)
-    }, 500)
+    }, 200)
   })
 });
