2ba6ef0b439ed54463ff5a30e71e654e92721ee3
obskera <vivifydecepi@gmail.com>
2022-10-14 21:23:29 -0700

    nav bug fix

---
+++ public/js/press2.js
@@ -0,0 +1,16 @@
+//handle unpress simulate animation
+// alert('connected')
+let elements = document.querySelectorAll('.unpress2')
+// if (!elements) { 
+//   const elements = document.querySelectorAll('.unpress')
+// }
+// const dummy = document.getElementById('dummy')
+Array.from(elements).forEach(function(ele) {
+  ele.addEventListener('click', () => {
+    ele.style.color = 'black'
+    setTimeout(() => {
+      ele.style.color = 'white'
+      // console.log('success!', ele)
+    }, 200)
+  })
+});
